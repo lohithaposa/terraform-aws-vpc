9ee3dd8b11895060e36fce1ad668215a9b77ebe3
Lohitha <posalohitha189@gmail.com>
2026-05-04 11:28:58 +0530

    terraform-vpc

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "project" {
 
 variable "environment" {
     type = string
-    validation {
-        condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
-        error_message = "Environments should be one of dev, qa, uat or prod"
+    # validation {
+    #     condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
+    #     error_message = "Environments should be one of dev, qa, uat or prod"
     }
-}
+
 
 variable "vpc_cidr" {
     type = string
@@ -33,4 +33,42 @@ variable "public_subnet_cidrs" {
 variable "public_subnet_tags" {
     default = {}
     type = map
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "private_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "public_route_table_tags" {
+    default = {}
+    type = map
+     
+}
+
+variable "private_route_table_tags" {
+    default = {}
+    type = map
+     
+}
+
+variable "database_route_table_tags" {
+    default = {}
+    type = map
+     
 }
